6927fbe22b99950c9f44470240bfa5b60f6e92c4
Arush Karnatak <karnatakarush@gmail.com>
2025-10-07 12:29:22 +0530

    #122 - Ganache to Anvil: main_misc.py
    * Migrate main_misc.py from Ganache to Anvil
    * file name changed from, main_misc.py to buy_v2_sweep_pay_portion.py
    
    ---------
    
    Co-authored-by: Kcodess2807 <karush2807@gmailcom>

--- integration_tests/buy_v2_sweep_pay_portion.py
+++ integration_tests/buy_v2_sweep_pay_portion.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 import time
 
-from eth_utils import keccak
 from web3 import (
     Account,
     Web3,
@@ -17,42 +16,40 @@ from uniswap_universal_router_decoder import (
 
 web3_provider = os.environ['WEB3_HTTP_PROVIDER_URL_ETHEREUM_MAINNET']
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-chain_id = 1337
-block_number = 18547231
+chain_id = 1
+block_number = 21839495
+init_amount = 10000 * 10**18
 gas_limit = 800_000
 
-account = Account.from_key(keccak(text="moo"))
-assert account.address == "0xcd7328a5D376D5530f054EAF0B9D235a4Fd36059"
-init_amount = 100 * 10**18
+account = Account.from_key('0xf7e96bcf6b5223c240ec308d8374ff01a753b00743b3a0127791f37f00c56514')
+assert account.address == '0x1e46c294f20bC7C27D93a9b5f45039751D8BCc3e'
 
-account_2 = Account.from_key(keccak(text="baa"))
-assert account_2.address == "0xdb45Eb8B663084225ec9bB814b827F5946Ca3665", f"Actual account_2 address is {account_2.address}"  # noqa
+account_2 = Account.from_key('0x75b69019be9c48a713678b028ee4dbafd81949126357b28ced1f616581ade9ac')
+assert account_2.address == '0x7b0ecEa560726C1c3FA0be132017c6f6C51526F2'
 
 erc20_abi = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]'  # noqa
 weth_abi = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]'  # noqa
 weth_address = Web3.to_checksum_address("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-
 uni_address = Web3.to_checksum_address("0x1f9840a85d5af5bf1d1762f925bdaddc4201f984")
 uni_contract = w3.eth.contract(address=uni_address, abi=erc20_abi)
 
-ur_address = Web3.to_checksum_address("0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B")
+ur_address = Web3.to_checksum_address("0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af")
 
 codec = RouterCodec()
 
 
-def launch_ganache():
-    ganache_process = subprocess.Popen(
-        f"""ganache
-        --logging.quiet='true'
-        --fork.url='{web3_provider}'
-        --fork.blockNumber='{block_number}'
-        --miner.defaultGasPrice='35000000000'
-        --wallet.accounts='{account.key.hex()}','{init_amount}'
-        """.replace("\n", " "),
+def launch_anvil():
+    anvil_process = subprocess.Popen(
+        " ".join([
+            "anvil",
+            f"--fork-url {web3_provider}",
+            f"--fork-block-number {block_number}",
+            "--mnemonic-seed-unsafe 8721345628937456298",
+        ]),
         shell=True,
     )
-    time.sleep(3)
-    parent_id = ganache_process.pid
+    time.sleep(2)
+    parent_id = anvil_process.pid
     return parent_id
 
 
@@ -67,13 +64,14 @@ def kill_processes(parent_id):
 
 
 def check_initialization():
-    assert w3.eth.chain_id == chain_id  # 1337
-    assert w3.eth.block_number == block_number + 1
+    assert w3.eth.chain_id == chain_id
+    assert w3.eth.block_number == block_number
     assert w3.eth.get_balance(account.address) == init_amount
-    assert w3.eth.get_balance(account_2.address) == 0
+    assert w3.eth.get_balance(account_2.address) == init_amount
+
     assert uni_contract.functions.balanceOf(account.address).call() == 0
     assert uni_contract.functions.balanceOf(account_2.address).call() == 0
-    print(" => Initialization: OK")
+    print(" => INITIALIZATION: OK")
 
 
 def send_transaction(value, encoded_data):
@@ -89,7 +87,7 @@ def send_transaction(value, encoded_data):
         "nonce": w3.eth.get_transaction_count(account.address),
         "data": encoded_data,
     }
-    raw_transaction = w3.eth.account.sign_transaction(trx_params, account.key).rawTransaction
+    raw_transaction = w3.eth.account.sign_transaction(trx_params, account.key).raw_transaction
     trx_hash = w3.eth.send_raw_transaction(raw_transaction)
     return trx_hash
 
@@ -97,9 +95,8 @@ def send_transaction(value, encoded_data):
 def buy_uni_with_fees():
     # Buying for 1 eth of uni from v2 and send 1% of out_amount to account_2
     v2_path = [weth_address, uni_address]
-    v2_in_amount = 1 * 10 ** 18  # 1 eth
-    out_amount = 381140129360952845496
-    v2_out_amount = int(out_amount * 0.995)  # 0.5% slippage
+    v2_in_amount = 1 * 10**18  # 1 eth
+    v2_out_amount = 267 * 10**18
     fees = 100  # 1%
     encoded_input = (
         codec
@@ -111,16 +108,24 @@ def buy_uni_with_fees():
         .sweep(FunctionRecipient.SENDER, uni_address, 0)
         .build(codec.get_default_deadline())
     )
-    trx_hash = send_transaction(v2_in_amount, encoded_input)
 
+    trx_hash = send_transaction(v2_in_amount, encoded_input)
     receipt = w3.eth.wait_for_transaction_receipt(trx_hash)
     assert receipt["status"] == 1  # trx success
     assert w3.eth.get_balance(account.address) < init_amount - v2_in_amount
     account_uni_balance = uni_contract.functions.balanceOf(account.address).call()
     account_2_uni_balance = uni_contract.functions.balanceOf(account_2.address).call()
-    assert account_uni_balance == int(out_amount * 0.99) - 1998, f"Actual account uni balance is {account_uni_balance}"
-    assert account_2_uni_balance == int(out_amount * 0.01) + 134, f"Actual account_2 uni balance is {account_2_uni_balance}"  # noqa
-    assert account_uni_balance + account_2_uni_balance == out_amount
+
+    # Validate both accounts received UNI and total matches expected ratio
+    total_uni = account_uni_balance + account_2_uni_balance
+    assert account_uni_balance > 0, f"Actual account uni balance is {account_uni_balance}"
+    assert account_2_uni_balance > 0, f"Actual account_2 uni balance is {account_2_uni_balance}"
+
+    # Validate 1% fee distribution (with small tolerance for rounding)
+    expected_account_2_amount = int(total_uni * 0.01)
+    assert abs(account_2_uni_balance - expected_account_2_amount) < 1000, \
+        f"Account 2 should get ~1% of total. Got {account_2_uni_balance}, expected ~{expected_account_2_amount}"
+
     print(" => BUY UNI WITH FEES: OK")
 
 
@@ -139,12 +144,12 @@ def print_success_message():
 
 
 def main():
-    ganache_pid = launch_ganache()
+    anvil_pid = launch_anvil()
     try:
         launch_integration_tests()
         print_success_message()
     finally:
-        kill_processes(ganache_pid)
+        kill_processes(anvil_pid)
 
 
 if __name__ == "__main__":
